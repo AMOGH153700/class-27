f4774c6a698be15cf9f3cf89a98cf259c694e39b
AMOGH <hanumanprasadkp@gmail.com>
2021-02-22 18:02:46 +0530

    comms

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
-
+const Constraint = Matter.Constraint;
+var chain;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
-
+var wooden;
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
 }
@@ -33,8 +34,13 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+    wooden= new Log(90,90,50,PI/2);
     bird = new Bird(100,100);
+    chain=new Chain(bird.body,wooden.body);
+     
+        
+    
+    
 
 }
 
@@ -58,7 +64,10 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-
+    wooden.display();
     bird.display();
     platform.display();
+    chain.display();
+  
+
 }
